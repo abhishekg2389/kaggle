c7a4a91e0a9d470021658f50369d447d28ae459a
Abhishek Gupta <gupta.abhii.92@gmail.com>
2016-02-05 18:16:05 +0530

    Create RUNME.R

---
+++ coupon/RUNME.R
@@ -0,0 +1,21 @@
+# requires following packages
+require(data.table)
+require(plyr)
+require(xgboost)
+require(Metrics) # for map@10
+require(pROC)
+require(foreach)
+require(doMC)
+require(ROCR)
+
+# set path
+setwd('[enter the path where you downloaded repo]') # setwd('~/Projects/Recruit-Coupon-Purchase/github-code/')
+
+# run code
+source('code/clean.R') # ~ 340 seconds
+source('code/functions.R')
+source('code/featurize.R') # TAKES A LOT OF TIME AND CONTAINS PARALLEL CODE!
+source('code/model_xgboost.R')
+
+# for BPR Matrix Factorization using MyMediaLite library
+source('code/model_BPRMFknn.R')
